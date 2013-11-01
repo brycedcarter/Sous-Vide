6b264e2e742e7c73ab48db8f2ffa2e290322a7b9
brycedcarter <brycedcarter@gmail.com>
2013-11-01 18:37:27 -0400

    working on debounce

--- sous_vide_controller/sous_vide_controller.ino
+++ sous_vide_controller/sous_vide_controller.ino
@@ -17,17 +17,83 @@
 LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 //========================================
 
+const int buttonPin = 2;
+const int refPin = A0;
+const int tempPin1 = A1
+
+bool displayState = true;
+bool referencing = false;
+int refVoltage = -1;
+int refTemp = -1;
+
+long lastDebounceTime = 0;
+long debounceDelay = 50;
+int buttonState;  
+int lastButtonState = HIGH;   
+
 void setup()
 {
-lcd.begin (20,4,LCD_5x8DOTS);
-lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+  pinMode(buttonPin, INPUT);
+  lcd.begin (20,4,LCD_5x8DOTS);
+  lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
 }
 
 void loop()
 {
-lcd.setBacklight(HIGH);
-lcd.home ();
-int sensorValue = analogRead(A0);
-float voltage = sensorValue * (5.0 / 1023.0);
-lcd.print(voltage);
+ 
+  int buttonReading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  } 
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      if(reading==HIGH)
+      {
+        referencing=false
+      }
+      else
+      {
+        referencing=true
+      }
+      buttonState = reading;
+
+      if (displayState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+  
+  lastButtonState = reading;
+  
+  
+  
+  //Turn the display backlight on or off
+  //========================================
+  if(displayState==true)
+  {
+    lcd.setBacklight(HIGH);
+  }
+  else
+  {
+    lcd.setBacklight(LOW);
+  }    
+  //========================================
+  lcd.home ();
+  
+  if(refTemp==-1)
+  {
+    lcd.print("Please set the reference temperature");
+  }
+  else
+  {
+    lcd.print(refTemp);
+  }
+
+  int buttonVal = analogRead(buttonPin);
+  if()
+  int sensorValue = analogRead(refPin);
+  float voltage = sensorValue * (5.0 / 1023.0);
+
 }
